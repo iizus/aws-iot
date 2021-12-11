8721a4aae3f4959a332cd4e36f22f5d11b1f2567
iizus <shota.iizuka@icloud.com>
2021-12-11 22:38:59 +0000

    modify

--- code/fleetprovisioning.py
+++ code/fleetprovisioning.py
@@ -1,7 +1,7 @@
 import argparse
+from msilib.schema import Feature
 from awscrt import io, mqtt
-from awsiot import iotidentity
-from awsiot import mqtt_connection_builder
+from awsiot import iotidentity, mqtt_connection_builder
 from concurrent.futures import Future
 import sys
 import threading
@@ -37,7 +37,8 @@ parser.add_argument("--templateParameters", help="Values for Template Parameters
 is_sample_done = threading.Event()
 args = parser.parse_args()
 
-io.init_logging(getattr(io.LogLevel, args.verbosity), 'stderr')
+__log_level = getattr(io.LogLevel, args.verbosity)
+io.init_logging(__log_level, 'stderr')
 
 createKeysAndCertificateResponse = None
 createCertificateFromCsrResponse = None
@@ -48,9 +49,9 @@ class LockedData:
         self.lock = threading.Lock()
         self.disconnect_called = False
 
-locked_data = LockedData()
 
 def __disconnect(mqtt_connection):
+    locked_data = LockedData()
     with locked_data.lock:
         if not locked_data.disconnect_called:
             print("Disconnecting...")
@@ -61,7 +62,7 @@ def __disconnect(mqtt_connection):
 
 # Function for gracefully quitting this sample
 def __error(msg_or_exception):
-    print("Exiting Sample due to exception.")
+    print("Exiting Sample due to exception")
     traceback.print_exception(
         msg_or_exception.__class__,
         msg_or_exception,
@@ -71,122 +72,109 @@ def __error(msg_or_exception):
     
 def on_disconnected(disconnect_future):
     # type: (Future) -> None
-    print("Disconnected.")
+    print("Disconnected")
     # Signal that sample is finished
     is_sample_done.set()
+
 
 def on_publish_register_thing(future):
     # type: (Future) -> None
     try:
         future.result() # raises exception if publish failed
-        print("Published RegisterThing request..")
+        print("Published RegisterThing request")
     except Exception as e:
-        print("Failed to publish RegisterThing request.")
+        print("Failed to publish RegisterThing request")
         __error(e)
 
-def on_publish_create_keys_and_certificate(future):
-    # type: (Future) -> None
+
+def on_publish_create_keys_and_certificate(future: Feature) -> None:
     try:
         future.result() # raises exception if publish failed
-        print("Published CreateKeysAndCertificate request..")
-
+        print("Published CreateKeysAndCertificate request")
     except Exception as e:
-        print("Failed to publish CreateKeysAndCertificate request.")
+        print("Failed to publish CreateKeysAndCertificate request")
         __error(e)
 
-def on_publish_create_certificate_from_csr(future):
-    # type: (Future) -> None
+
+def on_publish_create_certificate_from_csr(future: Feature) -> None:
     try:
         future.result() # raises exception if publish failed
-        print("Published CreateCertificateFromCsr request..")
+        print("Published CreateCertificateFromCsr request")
     except Exception as e:
-        print("Failed to publish CreateCertificateFromCsr request.")
+        print("Failed to publish CreateCertificateFromCsr request")
         __error(e)
 
-def createkeysandcertificate_execution_accepted(response):
-    # type: (iotidentity.CreateKeysAndCertificateResponse) -> None
+
+def createkeysandcertificate_execution_accepted(response: iotidentity.CreateKeysAndCertificateResponse) -> None:
     try:
         global createKeysAndCertificateResponse
         createKeysAndCertificateResponse = response
-        # print("Received a new message {}".format(createKeysAndCertificateResponse))
-        print(f'Certificate ID: {createKeysAndCertificateResponse.certificate_id}')
-
-        path = 'certs/client.pem'
-        write_file(path=f'{path}.crt', content=createKeysAndCertificateResponse.certificate_pem)
-        write_file(path=f'{path}.key', content=createKeysAndCertificateResponse.private_key)
+        print(f'Certificate ID: {response.certificate_id}')
+        __save_certs_based_on(response)
         return
     except Exception as e:
         __error(e)
 
 
-def write_file(path, content):
+def __save_certs_based_on(response: iotidentity.CreateKeysAndCertificateResponse) -> None:
+    path = 'certs/client.pem'
+    __save_file(path=f'{path}.crt', content=response.certificate_pem)
+    __save_file(path=f'{path}.key', content=response.private_key)
+
+
+def __save_file(path: str, content: str) -> None:
     with open(path, mode='w') as file:
         file.write(content)
         print(f'Saved {path}')
-        print(content)
 
 
-def createkeysandcertificate_execution_rejected(rejected):
-    # type: (iotidentity.RejectedError) -> None
-    __error("CreateKeysAndCertificate Request rejected with code:'{}' message:'{}' statuscode:'{}'".format(
-        rejected.error_code, rejected.error_message, rejected.status_code))
+def createkeysandcertificate_execution_rejected(response: iotidentity.ErrorResponse) -> None:
+    __print_rejected('CreateKeysAndCertificate', response)
 
-def createcertificatefromcsr_execution_accepted(response):
-    # type: (iotidentity.CreateCertificateFromCsrResponse) -> None
-    try:
-        global createCertificateFromCsrResponse
-        createCertificateFromCsrResponse = response
-        print("Received a new message {}".format(createCertificateFromCsrResponse))
-        global certificateOwnershipToken
-        certificateOwnershipToken = response.certificate_ownership_token
-        return
-    except Exception as e:
-        __error(e)
 
-def createcertificatefromcsr_execution_rejected(rejected):
-    # type: (iotidentity.RejectedError) -> None
-    __error(f"CreateCertificateFromCsr Request rejected with code:'{rejected.error_code}' message:'{rejected.error_message}' statuscode:'{rejected.status_code}'")
-
-def registerthing_execution_accepted(response):
-    # type: (iotidentity.RegisterThingResponse) -> None
+def registerthing_execution_accepted(response: iotidentity.RegisterThingResponse) -> None:
     try:
         global registerThingResponse
         registerThingResponse = response
-        print("Received a new message {registerThingResponse} ")
+        print(f"Received a new message {registerThingResponse}")
         return
-
     except Exception as e:
         __error(e)
 
-def registerthing_execution_rejected(rejected):
-    # type: (iotidentity.RejectedError) -> None
-    __error("RegisterThing Request rejected with code:'{}' message:'{}' statuscode:'{}'".format(
-        rejected.error_code, rejected.error_message, rejected.status_code))
+
+def registerthing_execution_rejected(response: iotidentity.ErrorResponse) -> None:
+    __print_rejected('RegisterThing', response)
+
+
+def __print_rejected(api: str, response: iotidentity.ErrorResponse) -> None:
+    __error(f"{api} request rejected with code: {response.error_code} message: {response.error_message} status code: {response.status_code}")
+
 
 # Callback when connection is accidentally lost.
-def on_connection_interrupted(connection, error, **kwargs):
-    print("Connection interrupted. error: {}".format(error))
+def on_connection_interrupted(connection, error, **kwargs) -> None:
+    print(f"Connection interrupted. Error: {error}")
 
 
 # Callback when an interrupted connection is re-established.
 def on_connection_resumed(connection, return_code, session_present, **kwargs):
-    print("Connection resumed. return_code: {} session_present: {}".format(return_code, session_present))
+    print(f"Connection resumed. return code: {return_code} session present: {session_present}")
 
     if return_code == mqtt.ConnectReturnCode.ACCEPTED and not session_present:
         print("Session did not persist. Resubscribing to existing topics...")
         resubscribe_future, _ = connection.resubscribe_existing_topics()
-
         # Cannot synchronously wait for resubscribe result because we're on the connection's event-loop thread,
         # evaluate result with a callback instead.
         resubscribe_future.add_done_callback(on_resubscribe_complete)
 
+
 def on_resubscribe_complete(resubscribe_future):
     resubscribe_results = resubscribe_future.result()
-    print("Resubscribe results: {}".format(resubscribe_results))
+    print(f"Resubscribe results: {resubscribe_results}")
 
     for topic, qos in resubscribe_results['topics']:
         if qos is None:
-            sys.__error("Server rejected resubscribe to topic: {}".format(topic))
+            sys.__error(f"Server rejected resubscribe to topic: {topic}")
+
 
 def waitForCreateKeysAndCertificateResponse():
     # Wait for the response.
@@ -197,6 +185,7 @@ def waitForCreateKeysAndCertificateResponse():
         print('Waiting... CreateKeysAndCertificateResponse: ' + json.dumps(createKeysAndCertificateResponse))
         loopCount += 1
         time.sleep(1)
+
 
 def waitForRegisterThingResponse():
     # Wait for the response.
@@ -333,19 +322,23 @@ def __publish_registerThing_topic_by(client):
 
 
 def __provision_by(mqtt_connection):
-    client = iotidentity.IotIdentityClient(mqtt_connection)
     try:
         # Subscribe to necessary topics.
         # Note that is **is** important to wait for "accepted/rejected" subscriptions
         # to succeed before publishing the corresponding "request".
-        __subscribe_CreateKeysAndCertificate_topics_by(client)
-        __subscribe_RegisterThing_topics_by(client)
-        __publish_CreateKeysAndCertificate_topic_by(client)
-        __publish_registerThing_topic_by(client)
+        client = iotidentity.IotIdentityClient(mqtt_connection)
+        __subscribe_and_pubrish_topics_by(client)
         print("Success")
         __disconnect(mqtt_connection)
     except Exception as e:
         __error(e)
+
+
+def __subscribe_and_pubrish_topics_by(client):
+    __subscribe_CreateKeysAndCertificate_topics_by(client)
+    __subscribe_RegisterThing_topics_by(client)
+    __publish_CreateKeysAndCertificate_topic_by(client)
+    __publish_registerThing_topic_by(client)
 
 
 def provision():
