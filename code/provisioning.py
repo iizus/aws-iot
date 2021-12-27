bd8062fab8fc8024e5200ba654ecca007d6763a2
iizus <shota.iizuka@icloud.com>
2021-12-27 00:41:22 +0000

    sepalate files

---
+++ code/provisioning.py
@@ -0,0 +1,55 @@
+from mqtt import MQTT
+from fleetprovisioning import FleetProvisioning
+from awscrt.mqtt import Connection
+
+
+class Provisioning:
+    def __init__(self, endpoint:str, template_name:str) -> None:
+        self.__mqtt:MQTT = MQTT(endpoint)
+        self.__fleet_provisioning:FleetProvisioning = FleetProvisioning(template_name)
+
+    def provision_thing_by(
+        self,
+        cert:str,
+        key:str,
+        ca:str,
+        client_id:str,
+        template_parameters:str,
+    ) -> str:
+        connection:Connection = self.__mqtt.connect_with(cert, key, ca, client_id)
+        thing_name:str = self.__fleet_provisioning.provision_thing_by(
+            connection,
+            template_parameters
+        )
+        self.__mqtt.disconnect(connection)
+        return thing_name
+
+
+def get_config_from(file_path:str) -> dict:
+    with open(file_path) as config_file:
+        from json import load
+        config:dict = load(config_file)
+        return config
+
+
+if __name__ == '__main__':
+    config:dict = get_config_from(file_path='config.json')
+
+    __fp = Provisioning(
+        endpoint = config.get('endpoint'),
+        template_name = config.get('template_name')
+    )
+
+    from uuid import uuid4
+    folder:str = 'certs'
+    claim:str = f'{folder}/claim.pem'
+    device_ID:str = str(uuid4())
+    print(f"Device ID: {device_ID}")
+
+    thing_name:str = __fp.provision_thing_by(
+        cert = f'{claim}.crt',
+        key = f'{claim}.key',
+        ca = f'{folder}/AmazonRootCA1.pem',
+        client_id = device_ID,
+        template_parameters = {"DeviceID": device_ID},
+    )
