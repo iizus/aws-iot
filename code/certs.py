818b2a5d84f0466e6fd7c7ceccc4007eb3685dfc
iizus <shota.iizuka@icloud.com>
2022-05-19 02:46:06 +0000

    add get_ca_path()

--- code/certs.py
+++ code/certs.py
@@ -1,6 +1,15 @@
 from typing import List
 from glob import glob
 
+    
+def get_ca_path() -> str:
+    '''
+    >>> get_ca_path()
+    'certs/AmazonRootCA1.pem'
+    '''
+    ca_path = __get_file_path_of(project_path='', file_type='pem')
+    return ca_path
+
 
 def get_certs_path_of(project_name:str) -> List[str]:
     '''
@@ -37,9 +46,21 @@ def __get_certs_path_of(project_name:str, cert_type:str) -> str:
     >>> __get_certs_path_of(project_name='test', cert_type='key')
     'certs/test/ad1b473789e53248ecd36eb7e6a888755d17bf21318b3fb3ca8bf79cbb636055-private.pem.key'
     '''
-    certs_path = f"certs/{project_name}/*.{cert_type}"
-    cert_path = glob(certs_path)[0]
+    cert_path = __get_file_path_of(
+        project_path = f'{project_name}/',
+        file_type = cert_type
+    )
     return cert_path
+
+
+def __get_file_path_of(project_path:str, file_type:str) -> str:
+    '''
+    >>> __get_file_path_of(project_path='', file_type='pem')
+    'certs/AmazonRootCA1.pem'
+    '''
+    files_path = f"certs/{project_path}*.{file_type}"
+    filr_path = glob(files_path)[0]
+    return filr_path
 
 
 if __name__ == '__main__':
