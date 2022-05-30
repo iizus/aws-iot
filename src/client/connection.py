9738248b304facf62c743ab27e952f86f4b58ead
iizus <shota.iizuka@icloud.com>
2022-05-30 10:40:46 +0000

    add print methods

--- src/client/connection.py
+++ src/client/connection.py
@@ -3,6 +3,10 @@ from awscrt import mqtt
 
 from src.fleet_provisioning.fleetprovisioning import FleetProvisioning
 
+def print_log(subject:str, message:str) -> None:
+    print(f"[{subject}] {message}")
+
+
 class Connection:
     def __init__(self, project_name:str, connection:mqtt.Connection) -> None:
         self.__project_name:str = project_name
@@ -21,9 +25,9 @@ class Connection:
 
     def disconnect(self) -> dict:
         client_id:str = self.__connection.client_id
-        print(f"[{client_id}] Disconnecting...")
+        print_log(subject=client_id, message="Disconnecting...")
         disconnect_result:dict = self.__connection.disconnect().result()
-        print(f"[{client_id}] Disconnected and Result: {disconnect_result}")
+        print_log(subject=client_id, message=f"Disconnected and Result: {disconnect_result}")
         return disconnect_result
 
 
@@ -45,7 +49,7 @@ import json
 class Topic:
     def print_recieved_message(topic:str, payload:str, dup:bool, qos:mqtt.QoS, retain:bool, **kwargs:dict) -> None:
         message:dict = json.loads(payload)
-        print(f"[{topic}] Recieved {message} by QoS{qos}, DUP: {dup} and Retain: {retain}")
+        print(f"[{topic}] Recieved {message} by QoS{qos}, DUP: {dup} and Retain message: {retain}")
 
 
     def __init__(
@@ -62,33 +66,54 @@ class Topic:
         self.__endpoint:str = f"{connection.host_name}:{connection.port}/{self.__topic}"
         self.__QoS:Literal = QoS
         self.__retain:bool = retain
-        print(f"[{self.client_id}] Set topic as {self.__topic} by QoS{self.__QoS} and Retain message: {self.__retain}")
+        print_log(subject=self.client_id, message=f"Set topic as {self.__topic} by QoS{self.__QoS} and Retain message: {self.__retain}")
 
 
     def publish(self, message:dict={'message': 'test'}) -> int:
         payload:str = json.dumps(message)
-        print(f"[{self.client_id}] Publishing... {payload} to {self.__endpoint} by QoS{self.__QoS} and Retain message: {self.__retain}")
+        self.__print_publish_log(verb='Publishing...', payload=payload)
         _, packet_id = self.__connection.publish(self.__topic, payload, self.__QoS, self.__retain)
-        print(f"[{self.client_id}] Published {payload} to {self.__endpoint} by QoS{self.__QoS}, Retain message: {self.__retain} and Packet ID: {packet_id}")
+        self.__print_publish_log(verb='Published', payload=payload, packet_id=packet_id)
         return packet_id
 
 
     def subscribe(self, callback=print_recieved_message) -> dict:
-        print(f"[{self.client_id}] Subscribing... {self.__endpoint} by QoS{self.__QoS} and Callback: {callback.__name__}")
+        self.__print_subscribe_log(verb='Subscribing...', QoS=self.__QoS, callback_name=callback.__name__)
         subscribe_future, packet_id = self.__connection.subscribe(
             self.__topic,
             self.__QoS,
             callback,
         )
         subscribe_result:dict = subscribe_future.result()
-        topic:str = subscribe_result.get('topic')
+        # topic:str = subscribe_result.get('topic')
         QoS:int = subscribe_result.get('qos')
-        print(f"[{self.client_id}] Subscribed {self.__endpoint} Topic: {topic} by QoS{QoS}, Callback: {callback.__name__} and Packet ID: {packet_id}")
+        self.__print_subscribe_log(verb='Subscribed', QoS=QoS, callback_name=callback.__name__, packet_id=packet_id)
         return subscribe_result
 
 
     def unsubscribe(self) -> int:
-        print(f"[{self.client_id}] Unsubscribing... {self.__endpoint}")
+        self.__print_unsubscribe_log(verb='Unsubscribing...')
         _, packet_id = self.__connection.unsubscribe(self.__topic)
-        print(f"[{self.client_id}] Unsubscribed {self.__endpoint} and Packet ID: {packet_id}")
+        self.__print_unsubscribe_log(verb='Unsubscribed', packet_id=packet_id)
         return packet_id
+
+
+    def __print_publish_log(self, verb:str, payload:str, packet_id:int=None) -> None:
+        message:str = f"{payload} to {self.__endpoint} by QoS{self.__QoS}, Retain message: {self.__retain}"
+        self.__print_pubsub_log(verb, message, packet_id)
+
+
+    def __print_subscribe_log(self, verb:str, QoS:mqtt.QoS, callback_name:str, packet_id:int=None) -> None:
+        self.__print_pubsub_log(verb=verb, message=f"{self.__endpoint} by QoS{QoS}, Callback: {callback_name}", packet_id=packet_id)
+
+
+    def __print_unsubscribe_log(self, verb:str, packet_id:int=None) -> None:
+        self.__print_pubsub_log(verb=verb, message=self.__endpoint, packet_id=packet_id)
+
+
+    def __print_pubsub_log(self, verb:str, message:str, packet_id:int=None) -> None:
+        self.__print_log(verb, f"{message} and Packet ID: {packet_id}")
+
+
+    def __print_log(self, verb:str, message:str) -> None:
+        print_log(subject=self.client_id, message=f"{verb} {message}")
