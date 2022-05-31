5bbabb6c15d5b478cf088b6ff0956a7236451186
iizus <shota.iizuka@icloud.com>
2022-05-31 01:11:06 +0000

    add port.py

--- src/client/account.py
+++ src/client/account.py
@@ -1,33 +1,10 @@
-from awscrt.http import HttpProxyOptions
-
-class Proxy:
-    def __init__(self, name:str, ca:str, number:int=8883, proxy:HttpProxyOptions=None) -> None:
-        self.name:str = name
-        self.ca:str = ca
-        self.port:int = number
-        self.proxy:HttpProxyOptions = proxy
-
-
-
-class Port:
-    def __init__(self, name:str, ca:str, number:int=8883) -> None:
-        self.name:str = name
-        self.ca:str = ca
-        self.port:int = number
-        self.proxy:HttpProxyOptions = None
-        print(f"[Endpoint] Set to {self.name}:{self.port}")
-
-
-    def set_proxy(self, host:str, port:int=443) -> Proxy:
-        proxy:HttpProxyOptions = HttpProxyOptions(host, port)
-        print(f"[Endpoint ]Set HTTP proxy as {host}:{port} for {self.name}:{self.port}")
-        return Proxy(self.name, self.ca, self.port, proxy)
-
-
-
-from src.client.client import Project, Client
 from threading import Event
+from awscrt.http import HttpProxyOptions
+from src.client.client import Project, Client
 from src.client.connection import Topic
+from src.client.port import Port
+
+
 
 class Endpoint:
     from awscrt import mqtt
