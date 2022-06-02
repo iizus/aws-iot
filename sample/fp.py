1a199428c96368875c0366264476b7918c5adc09
iizus <shota.iizuka@icloud.com>
2022-06-02 10:04:39 +0000

    remove files

--- sample/fp.py
+++ sample/fp.py
@@ -4,12 +4,11 @@ current_dir:str = path[0]
 parent_dir:str = dirname(current_dir)
 path.append(parent_dir)
 
-from src.client.account import Account, Endpoint
+from src.client.account import Endpoint, get_endpoint_of
 from src.client.client import Project, Client
 
 
-test_env:Account = Account(name='test')
-test_virginia:Endpoint = test_env.get_endpoint_of(region='us-east-1')
+test_virginia:Endpoint = get_endpoint_of(account_name='test')
 
 test:Project = Project(name='test')
 test_subscriber:Client = test.create_client(client_id='client1')
