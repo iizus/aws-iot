2e651d9a0fe86664aa3dd90b8d1bdaddef915d44
iizus <shota.iizuka@icloud.com>
2022-06-03 12:23:55 +0000

    fix

--- src/fleet_provisioning/fleetprovisioning.py
+++ src/fleet_provisioning/fleetprovisioning.py
@@ -12,9 +12,7 @@ class FleetProvisioning:
     
     def __init__(self, template_name:str) -> None:
         self.__template_name:str = template_name
-        self.__createKeysAndCertificateResponse:iotidentity.CreateKeysAndCertificateResponse = None
-        self.__registerThingResponse:iotidentity.RegisterThingResponse = None
-        self.__thing_name:str = None
+        self.__clear_members()
 
 
     def provision_thing(
@@ -23,10 +21,17 @@ class FleetProvisioning:
         template_parameters:str,
         thing_name:str = str(uuid4()),
     ) -> str:
+        self.__clear_members()
         self.__thing_name:str = thing_name
         provisioned_thing_name:str = self.__provision_by(connection, template_parameters)
         self.__print_log(verb='Success', message=f"fleet provisioning of {provisioned_thing_name}")
         return provisioned_thing_name
+
+
+    def __clear_members(self) -> None:
+        self.__createKeysAndCertificateResponse:iotidentity.CreateKeysAndCertificateResponse = None
+        self.__registerThingResponse:iotidentity.RegisterThingResponse = None
+        self.__thing_name:str = None
 
 
     def __provision_by(self, connection:Connection, template_parameters:str) -> str:
