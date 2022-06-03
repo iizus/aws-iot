94c09b3828c2094069ea5b0ba420db8c6620ef51
iizus <shota.iizuka@icloud.com>
2022-06-03 01:07:04 +0000

    update

--- sample/fp.py
+++ sample/fp.py
@@ -4,14 +4,16 @@ current_dir:str = path[0]
 parent_dir:str = dirname(current_dir)
 path.append(parent_dir)
 
-from src.client.account import Endpoint, get_endpoint_of
-from src.client.client import Project, Client
 
+
+from src.client.account import Endpoint, get_endpoint_of
+from src.client.client import Project
 
 test_virginia:Endpoint = get_endpoint_of(account_name='test')
-
+fp:Endpoint = test_virginia.set_FP(template_name='ec2')
 test:Project = Project(name='test')
-test_subscriber:Client = test.create_client(client_id='client1')
-test_publisher:Client = test_virginia.provision_thing(template_name='ec2')
 
-test_virginia.check_communication_between(publisher=test_publisher, subscriber=test_subscriber)
+test_virginia.check_communication_between(
+    publisher = test.create_client(client_id='client1'),
+    subscriber = fp.provision_thing(),
+)
