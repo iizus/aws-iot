abcef360d5c9ad99383a68cd04377e8c93c06ff7
iizus <shota.iizuka@icloud.com>
2022-10-28 01:51:51 +0000

    update fp

--- sample/fleet_provisioning.py
+++ sample/fleet_provisioning.py
@@ -9,10 +9,10 @@ path.append(parent_dir)
 from src.client.account import Endpoint, get_endpoint_of
 from src.client.client import Project
 
-test_virginia:Endpoint = get_endpoint_of(account_name='test')
-fp:Endpoint = test_virginia.set_FP(template_name='simple')
+test_virginia:Endpoint = get_endpoint_of(account_name='aws-iot')
+fp:Endpoint = test_virginia.set_FP(template_name='aws-iot')
 
-test:Project = Project(name='test')
+test:Project = Project(name='aws-iot')
 provisioning_thing_name:str = 'before_provisioned_thing'
 template_parameters:dict = {'DeviceID': provisioning_thing_name}
 
