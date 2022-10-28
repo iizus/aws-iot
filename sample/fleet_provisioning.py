d7b5cd81185e51aeae96d77fbd5e8a383bc9975d
iizus <shota.iizuka@icloud.com>
2022-10-28 08:29:30 +0000

    modify

--- sample/fleet_provisioning.py
+++ sample/fleet_provisioning.py
@@ -1,3 +1,4 @@
+from multiprocessing.connection import Client
 from sys import path
 from os.path import dirname
 current_dir:str = path[0]
@@ -5,23 +6,33 @@ parent_dir:str = dirname(current_dir)
 path.append(parent_dir)
 
 
-
+import datetime
 from src.client.account import Endpoint, get_endpoint_of
 from src.client.client import Project
+
+def get_current_time(timezone: datetime.timezone) -> str:
+    now = datetime.datetime.now(tz=timezone)
+    thing_name:str = now.strftime('%Y-%m-%dT%H-%M-%S')
+    return thing_name
+
 
 test_virginia:Endpoint = get_endpoint_of(account_name='isengard')
 fp:Endpoint = test_virginia.set_FP(template_name='aws-iot')
 
+thing_name_key:str = 'device_id'
+nine_hours:datetime.timedelta = datetime.timedelta(hours=9)
+JST:datetime.timezone = datetime.timezone(nine_hours)
+
+template_parameters:dict = {
+    thing_name_key: get_current_time(timezone=JST)
+}
+provisioned_thing = fp.provision_thing(
+    template_parameters = template_parameters,
+    name = template_parameters.get(thing_name_key)
+)
+
 aws_iot:Project = Project(name='isengard')
-# provisioning_thing_name:str = 'fddddfd'
-
-import datetime
-now = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9)))
-provisioning_thing_name:str = now.strftime('%Y-%m-%dT%H-%M-%S')
-
-template_parameters:dict = {'device_id': provisioning_thing_name}
-
 test_virginia.check_communication_between(
     publisher = aws_iot.create_client(client_id='client1'),
-    subscriber = fp.provision_thing(template_parameters, provisioning_thing_name),
+    subscriber = provisioned_thing,
 )
