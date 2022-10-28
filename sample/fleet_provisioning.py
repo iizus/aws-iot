e4affcc9039e043729008992b5fb96f066f64c0a
iizus <shota.iizuka@icloud.com>
2022-10-28 08:40:36 +0000

    move func

--- sample/fleet_provisioning.py
+++ sample/fleet_provisioning.py
@@ -6,25 +6,19 @@ parent_dir:str = dirname(current_dir)
 path.append(parent_dir)
 
 
-import datetime
 from src.client.account import Endpoint, get_endpoint_of
 from src.client.client import Project
-
-def get_current_time(timezone: datetime.timezone) -> str:
-    now = datetime.datetime.now(tz=timezone)
-    thing_name:str = now.strftime('%Y-%m-%dT%H-%M-%S')
-    return thing_name
+from src.fleet_provisioning.util import get_current_time
 
 
 test_virginia:Endpoint = get_endpoint_of(account_name='isengard')
 fp:Endpoint = test_virginia.set_FP(template_name='aws-iot')
 
+
 thing_name_key:str = 'device_id'
-nine_hours:datetime.timedelta = datetime.timedelta(hours=9)
-JST:datetime.timezone = datetime.timezone(nine_hours)
 
 template_parameters:dict = {
-    thing_name_key: get_current_time(timezone=JST)
+    thing_name_key: get_current_time()
 }
 provisioned_thing = fp.provision_thing(
     template_parameters = template_parameters,
--- src/fleet_provisioning/util.py
+++ src/fleet_provisioning/util.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 from sys import exc_info
 from traceback import print_exception
 from awsiot import iotidentity
@@ -6,13 +7,21 @@ from src.utils.util import remove
 
 
 
-def save_certs_in(dir:str, response:iotidentity.CreateKeysAndCertificateResponse, thing_name:str) -> str:
+def save_certs_in(
+    dir:str,
+    response:iotidentity.CreateKeysAndCertificateResponse,
+    thing_name:str
+) -> str:
     path:str = __get_certs_path_based_on(thing_name, response.certificate_id, dir)
     __save_certs_at(path, response)
     return path
 
 
-def __get_certs_path_based_on(thing_name:str, certificate_id:str, dir:str='certs/fleet_provisioning/individual') -> str:
+def __get_certs_path_based_on(
+    thing_name:str,
+    certificate_id:str,
+    dir:str = 'certs/fleet_provisioning/individual'
+) -> str:
     folder:str = f'{dir}/{thing_name}'
     dir_path:str = __create(folder)
     path:str = f"{dir_path}/{certificate_id}.pem"
@@ -42,3 +51,13 @@ def error(msg_or_exception:Exception) -> None:
         msg_or_exception,
         exc_info()[2],
     )
+
+
+
+nine_hours:datetime.timedelta = datetime.timedelta(hours=9)
+JST:datetime.timezone = datetime.timezone(nine_hours)
+
+def get_current_time() -> str:
+    now = datetime.datetime.now(JST)
+    current_time:str = now.strftime('%Y-%m-%dT%H-%M-%S')
+    return current_time
