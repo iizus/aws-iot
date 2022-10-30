2e712c19b115c6b9c601026b19b760edc882f714
iizus <shota.iizuka@icloud.com>
2022-10-30 04:23:29 +0000

    update

--- src/client/client.py
+++ src/client/client.py
@@ -98,17 +98,3 @@ def __on_resubscribe_complete(resubscribe_future:Future) -> None:
 # Callback when connection is accidentally lost.
 def on_connection_interrupted(error) -> None:
     print(f"Connection interrupted. Error: {error}")
-
-
-
-# class Project:
-#     def __init__(self, name:str='test') -> None:
-#         self.__name:str = name
-
-#     def create_client(self, client_id:str='client1', cert_dir:str='') -> Client:
-#         cert_dir:str = f'{self.__name}/{cert_dir}{client_id}'
-#         return Client(
-#             project_name = self.__name,
-#             id = self.__name if client_id == '' else client_id,
-#             cert = Cert(cert_dir)
-#         )
--- src/client/project.py
+++ src/client/project.py
@@ -1,12 +1,13 @@
 from src.client.certs import Cert
 from src.client.client import Client
+from src.fleet_provisioning.util import get_current_time
 
 
 class Project:
     def __init__(self, name:str='test') -> None:
         self.__name:str = name
 
-    def create_client(self, client_id:str='client1', cert_dir:str='') -> Client:
+    def create_client(self, client_id:str=get_current_time(), cert_dir:str='') -> Client:
         cert_dir:str = f'{self.__name}/{cert_dir}{client_id}'
         return Client(
             project_name = self.__name,
