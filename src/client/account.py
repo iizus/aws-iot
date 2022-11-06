0065efa7196483867a5e81160e76dd02e32ccf88
iizus <shota.iizuka@icloud.com>
2022-11-06 07:47:02 +0000

    remove comment

--- src/client/account.py
+++ src/client/account.py
@@ -15,22 +15,3 @@ class Account:
         endpoints:dict = util.load_json(endpoint_file_path)
         self.endpoint_prefix:str = endpoints.get(name)
         util.print_log(subject='Account', verb='Set', message=f"to {name}")
-
-
-    # def get_account(self) -> str:
-    #     return self.__endpoint_prefix
-
-
-
-#     def get_endpoint_of(self, region_name:str=config.get('REGION_NAME')) -> Endpoint:
-#         name:str = f'{self.__endpoint_prefix}-ats.iot.{region_name}.amazonaws.com'
-#         return Endpoint(name)
-
-
-# def get_endpoint(
-#     account_name:str = config.get('ACCOUNT_NAME'),
-#     region_name:str = config.get('REGION_NAME'),
-# ) -> Endpoint:
-#     account:Account = Account(account_name)
-#     endpoint:Endpoint = account.get_endpoint_of(region_name)
-#     return endpoint
--- src/client/endpoint.py
+++ src/client/endpoint.py
@@ -27,13 +27,3 @@ class Endpoint:
             verb = 'Set',
             message = f"to {self.endpoint}, CA path: {self.ca_path}"
         )
-
-    # def set_ca(self, type:str='RSA2048'):
-    #     return Endpoint(name=self.name, ca=type, port=self.port, proxy=self.proxy)
-
-    # def set_port(self, number:int=8883):
-    #     return Endpoint(name=self.name, ca=self.ca, port=number, proxy=self.proxy)
-
-    # def set_proxy(self, host:str, port:int):
-    #     options:HttpProxyOptions = HttpProxyOptions(host_name=host, port=port)
-    #     return Endpoint(name=self.name, ca=self.ca, port=self.port, proxy=options)
